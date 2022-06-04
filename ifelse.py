3f59df8cb9ed4322e845c5da39cbae74a9e2987d
ben <ben.transinh@gmail.com>
2022-06-04 14:28:24 +0700

    learn if else

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+print("Nhập điểm trung bình :")
+dib = float(input())
+if dib > 9:
+    print("Xếp loại giỏi")
+elif dib >= 7:
+    print("Xếp loại khá")
+elif dib >=5:
+    print("Xếp loại trung bình ")
+else :
+    print("Xếp loại kém ")
