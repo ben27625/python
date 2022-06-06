7b6144861759959bbf3f747d803ea832f782909e
ben <ben.transinh@gmail.com>
2022-06-06 22:07:27 +0700

    handl in

---
+++ while.py
@@ -0,0 +1,14 @@
+# print("Nhập 1 số [1 .. 10 ]")
+# x = -1
+# while x < 1 or x > 10:
+#     x =  int(input("NHập 1 số :"))
+# print(x**2)
+print("Nhập N")
+n = int(input())
+s = 0
+i = 1
+while i<=n:
+    s = s+1
+    i = i +1
+
+print("Total = ", s)
